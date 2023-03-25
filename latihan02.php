1bc362893aee9f932dd350180d56b6ae11358bbd
Diantarii <127836891+Diantarii@users.noreply.github.com>
2023-03-25 08:29:12 +0700

    Pertemuan 2

---
+++ latihan02.php
@@ -0,0 +1,12 @@
+<?php
+    $angka= 3;
+
+    echo "Nilai Angka: <strong>".$angka."</strong>";
+
+    if($angka>4){
+        //kondisi true
+        echo"<br>Nilai Angka Lebih Dari 4";
+    }else{
+        //false
+        echo "<br>Nilai Angka Kurang Dari 5";
+    }
